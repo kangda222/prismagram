398a517668dd7c30507de97fdb84e9e1415c6cfd
DaeunKang <jeangrey@xhiftcorp.com>
2019-04-08 15:44:24 +0900

    seeFullPost Refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -1,10 +1,34 @@
-export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment{
+export const USER_FRAGMENT = `
+    id
+    username
+`;
+
+export const COMMENT_FRAGMENT = `    
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }    
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
         id
-        text
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
         user {
-            username
+            ${USER_FRAGMENT}
         }
     }
-    
-  `;
+`;
